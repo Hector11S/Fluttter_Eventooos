dcb6bca355ef62325d4fd1bd348f9c4793fe5d52
SabillonJ <161340097+SabillonJ@users.noreply.github.com>
2024-04-24 22:12:49 -0700

    menu

--- E-commerce-Complete-Flutter-UI-master (2)/E-commerce-Complete-Flutter-UI-master/lib/screens/home/components/home_header.dart
+++ E-commerce-Complete-Flutter-UI-master (2)/E-commerce-Complete-Flutter-UI-master/lib/screens/home/components/home_header.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 import '../../cart/cart_screen.dart';
 import 'icon_btn_with_counter.dart';
@@ -11,29 +12,36 @@ class HomeHeader extends StatelessWidget {
     Key? key,
   }) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: const Color.fromARGB(255, 255, 255, 255), 
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            const Expanded(child: SearchField()),
-            const SizedBox(width: 16),
-            IconBtnWithCounter(
-              svgSrc: "assets/icons/Cart Icon.svg",
-              press: () => Navigator.pushNamed(context, CarritoScreen.routeName),
-            ),
-            const SizedBox(width: 8),
-            IconBtnWithCounter(
-              svgSrc: "assets/icons/User.svg",
-              press: () => Navigator.pushNamed(context, SignInScreen.routeName),
-            ),
-          ],
-        ),
+@override
+Widget build(BuildContext context) {
+  return Container(
+    color: const Color.fromARGB(255, 255, 255, 255), 
+    child: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Expanded(child: SearchField()),
+          const SizedBox(width: 16),
+          PopupMenuButton(
+            icon: SvgPicture.asset("assets/icons/User.svg"),
+            itemBuilder: (context) => [
+              PopupMenuItem(
+                child: Text("Login"),
+                onTap: () {
+                  Navigator.pushNamed(context, SignInScreen.routeName);
+                },
+              ),
+            ],
+          ),
+          const SizedBox(width: 8),
+          IconBtnWithCounter(
+            svgSrc: "assets/icons/Cart Icon.svg",
+            press: () => Navigator.pushNamed(context, CarritoScreen.routeName),
+          ),
+        ],
       ),
-    );
-  }
+    ),
+  );
+}
 }
