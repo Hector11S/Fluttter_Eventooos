7dd6781ec5d7cfde87c3bcf5c53865957e24a5d4
SabillonJ <161340097+SabillonJ@users.noreply.github.com>
2024-04-26 03:38:51 -0700

    gdfgdf

--- E-commerce-Complete-Flutter-UI-master (2)/E-commerce-Complete-Flutter-UI-master/lib/screens/Carrito/RealizarPedido.dart
+++ E-commerce-Complete-Flutter-UI-master (2)/E-commerce-Complete-Flutter-UI-master/lib/screens/Carrito/RealizarPedido.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:shop_app/models/PedidosViewModel.dart';
 import 'package:http/http.dart' as http;
+import 'package:shop_app/screens/init_screen.dart';
 import 'package:shop_app/screens/sign_in/sign_in_screen.dart';
 import 'package:shop_app/screens/sign_up/components/sign_up_form.dart';
 
@@ -426,7 +427,7 @@ ElevatedButton(
   onPressed: () {
     if (_formKey.currentState!.validate()) {
       _RegistroCliente();
-      Navigator.pushNamed(context, SignInScreen.routeName);
+      Navigator.pushNamed(context, InitScreen.routeName);
     }
   },
   child: const Text("Guardar"),
